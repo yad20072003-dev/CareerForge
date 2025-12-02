3b13dfbfd303d6ba7aa6bd8e4477f9b16964ba75
yad20072003-dev <yad20072003@gmail.com>
2025-12-02 04:41:40 +0300

    Add various states for career and resume processes

--- states.py
+++ states.py
@@ -1,1 +1,34 @@
+from aiogram.fsm.state import StatesGroup, State
 
+
+class CareerState(StatesGroup):
+    waiting_for_input = State()
+
+
+class ResumeCreateState(StatesGroup):
+    waiting_for_input = State()
+
+
+class ResumeCheckState(StatesGroup):
+    waiting_for_resume = State()
+
+
+class MockInterviewState(StatesGroup):
+    waiting_for_dialog = State()
+
+
+class InterviewPlanState(StatesGroup):
+    waiting_for_info = State()
+
+
+class SoftSkillsState(StatesGroup):
+    waiting_for_answers = State()
+
+
+class VacancyMatchState(StatesGroup):
+    waiting_for_vacancy = State()
+    waiting_for_profile = State()
+
+
+class CoursesState(StatesGroup):
+    waiting_for_info = State()
