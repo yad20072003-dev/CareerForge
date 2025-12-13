8c4e59554b543b4270bfcb236ffd1ba790f37c50
yad20072003-dev <yad20072003@gmail.com>
2025-12-13 17:02:11 +0300

    Delete services/courses_service.py

--- services/courses_service.py
+++
@@ -1,9 +0,0 @@
-from services.ai import ai_answer
-from prompts.courses import COURSE_RECOMMEND_SYSTEM_PROMPT
-
-
-async def course_recommendations(user_payload: str) -> str:
-    return await ai_answer(
-        system_prompt=COURSE_RECOMMEND_SYSTEM_PROMPT,
-        user_prompt=user_payload
-    )
